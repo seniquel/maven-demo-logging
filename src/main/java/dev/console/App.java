94ae122ded9790b2f3ac4d19a946467ed521e478
Léo SENIQUE <leo.senique@gmail.com>
2020-06-23 11:35:46 +0200

    ajout TP logging

---
+++ src/main/java/dev/console/App.java
@@ -0,0 +1,17 @@
+package dev.console;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.service.AppService;
+
+public class App {
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+	public static void main(String[] args) {
+		LOG.info("Bonjour Slf4J !");
+		LOG.info("Implementation Logback");
+		new AppService().executer("AppService fonctionne !");
+
+	}
+
+}
